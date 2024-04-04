de3ea2c60ee8bd484d3e876d5dc06473afc5a4d0
glapettina <guillermo@glsoftware.com.ar>
2024-04-04 18:52:36 -0300

    commit inicial

---
+++ vistas/modulos/5-proyectos-edfisica.php
@@ -0,0 +1,378 @@
+<?php
+  
+
+    $item = null;
+    $valor = null;   
+
+    $cursos = ControladorCursos::ctrMostrarCursos($item, $valor);
+
+
+?>       
+
+
+  <div class="content-wrapper">
+    
+    <section class="content-header">
+      <h1>
+        5to. Año - Educación Física - Diseño y Gestión de Proyectos
+        
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">5to. Año - Educación Física - Diseño y Gestión de Proyectos</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+
+        <?php
+
+          if ($_SESSION["perfil"] != "Preceptor") {
+            
+              echo '<div class="box-header with-border">
+                        
+                <button type="submit" class="btn btn-danger" idCurso=11 idCurso2=12 idCurso3=16 idCurso4=17 tabla="quinto" data-toggle="modal" data-target="#modalCopiaSaberes">
+                  
+                  Copia Saberes
+                </button>
+
+                <button class="btn btn-primary btnInformeArea" area="proyectos" periodo="'.$_SESSION['periodo'].'" idCurso=11 idCurso2=12 idCurso3=16 idCurso4=17 modalidad="Educación Física" tabla="quinto" informe="informe-area-orientacion">
+                  
+                  Informes Curso
+                </button>
+                
+              </div>';
+
+          }
+
+        ?>
+
+
+
+        <div class="box-body">
+          
+          <table class="table table-bordered table-striped dt-responsive tablas">
+            
+            <thead>
+              
+              <tr>
+                
+                <th style="width: 10px">#</th>
+                <th>DNI</th>
+                <th>Apellidos y Nombres</th>
+                <th>Curso</th>
+                <th>Turno</th>
+                <th>Acciones</th>
+
+              </tr> 
+
+            </thead>
+            <tbody>
+
+              <?php
+
+                  $item = "id_curso";
+                  $valor1 = 11;
+                  $valor2 = 12;
+                  $valor3 = 16;
+                  $valor4 = 17;
+                  $tabla = "quinto";
+                  $periodo = $_SESSION["periodo"];
+                  $modalidad = "Educación Física";
+                  $verifica = true;
+
+
+                  $informes = ControladorInformes::ctrMostrarInformesOrientacion($item, $valor1, $valor2, $valor3, $valor4, $tabla, $periodo, $modalidad, $verifica);
+
+
+                  foreach ($informes as $key => $value) {
+                    
+                    echo '<tr>
+                
+                          <td>'.($key+1).'</td>
+                          <td>'.$value["documento"].'</td>
+                          <td>'.$value["nombre"].'</td>';
+
+                          $item = "id";
+                          $valor = $value["id_curso"];
+
+                          $curso = ControladorCursos::ctrMostrarCursos($item, $valor);
+
+                          echo '<td>'.$curso["nombre"].'</td>  
+
+                          <td>'.$curso["turno"].'</td>';
+
+
+                          echo '<td>';
+
+
+                          if ($_SESSION["perfil"] != "Preceptor") {
+
+                            
+                            echo'<div class="btn-group">
+
+                              <button class="btn btn-warning btnEditarInformeProyectos" nombreAlumno="'.$value["nombre"].'" tabla="quinto" periodo="'.$_SESSION['periodo'].'" idAlumno="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarInforme"><i class="fa fa-pencil"></i></button>
+                              
+                            </div>';
+                            
+                             
+                            }
+
+
+                            echo '<div class="btn-group">
+                                
+                              <button class="btn btn-primary btnImprimirInformeIndividual" informe="informe-individual" periodo="'.$_SESSION['periodo'].'" tabla="quinto" area="proyectos" idAlumno="'.$value["id"].'" data-toggle="modal" data-target="#modalImprimirInformeIndividual"><i class="fa fa-print"></i></button>
+                              
+                            </div>
+
+                          </td>
+
+                        </tr> ';
+                  }
+
+
+              ?>
+
+                           
+
+            </tbody>
+
+          </table>
+
+        </div>
+        
+        
+      </div>
+
+    </section>
+  </div>
+
+
+  
+
+
+<!--=====================================
+  MODAL EDITAR INFORME
+  ======================================-->
+
+
+<div id="modalEditarInforme" class="modal fade" role="dialog">
+
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" id="modalInforme">
+
+          <!--=====================================
+          CABEZA DEL MODAL
+          ======================================-->
+
+          <div class="modal-header" style="background: #3c8dbc; color: white">
+
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+           
+            <h4 class="modal-title" id="alumnoEdicion"></h4>
+
+          </div>
+
+          <!--=====================================
+          CUERPO DEL MODAL
+          ======================================-->
+
+          <div class="modal-body">
+
+            <div class="box-body">
+
+              <!-- ENTRADA PARA LOS SABERES -->          
+              
+              <div class="form-group">
+                
+                  <label for="saberesProyectos">Saberes</label>
+                      <textarea class="form-control" cols="80" rows="6" id="saberesProyectos" name="saberesProyectos">
+                  </textarea>
+              </div>
+
+        <!-- ENTRADA PARA LA APRECIACION --> 
+
+              
+              <div class="form-group">
+                
+                <div class="input-group">
+                  
+                  <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                  
+                  <select class="form-control input-lg" id="apreciaProyectos" name="apreciaProyectos">
+
+                  <option value="">Apreciación Cualitativa</option>
+                  <option value="Se Apropió de los Saberes">Se Apropió de los Saberes</option>
+                  <option value="En Proceso de Apropiación de Saberes">En Proceso de Apropiación de Saberes</option>
+                  <option value="No se Apropió de los Saberes">No se Apropió de los Saberes</option>
+                                        
+
+                  </select>
+
+                </div>
+
+              </div>
+
+              <!-- ENTRADA PARA LA ASISTENCIA --> 
+
+              
+              <div class="form-group">
+                
+                <div class="input-group">
+                  
+                  <span class="input-group-addon"><i class="fa fa-th"></i></span>
+                  <input class="form-control input-lg" type="number" name="asistenciaProyectos" id="asistenciaProyectos" placeholder="Ingresar asistencia (%)" required>
+
+                </div>
+
+              </div>
+
+
+              <div class="form-group">          
+                  <label for="observaProyectos">Observaciones</label>
+                      <textarea class="form-control" cols="80" rows="3" id="observaProyectos" name="observaProyectos">
+                  </textarea>
+              </div>
+
+
+            </div>
+            
+          </div>
+          
+          <!--=====================================
+          PIE DEL MODAL
+          ======================================-->
+
+
+
+          <div class="modal-footer">
+
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+            <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+
+              <input type="hidden" name="idAlumno" id="idAlumno"> 
+
+          </div>       
+
+         <?php
+
+              $tabla = "quinto";
+              $curso = "5-proyectos-edfisica";
+
+              // $ncurso1 = 7;
+              // $ncurso2 = 8;
+              // $periodo = $_SESSION["periodo"];
+
+              $editarInforme = new ControladorInformes();
+              $editarInforme -> ctrEditarInformeProyectos($tabla, $curso);
+
+          ?>
+
+    </form>
+
+          
+    </div>
+
+  </div>
+</div>
+
+
+<!--=====================================
+  MODAL COPIA SABERES
+  ======================================-->
+
+
+<div id="modalCopiaSaberes" class="modal fade" role="dialog">
+
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+          <!--=====================================
+          CABEZA DEL MODAL
+          ======================================-->
+
+          <div class="modal-header" style="background: #3c8dbc; color: white">
+
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+           
+              <h4 class="modal-title">Copia Saberes</h4>
+
+          </div>
+
+          <!--=====================================
+          CUERPO DEL MODAL
+          ======================================-->
+
+          <div class="modal-body">
+
+            <div class="box-body">
+
+              <!-- ENTRADA PARA LOS SABERES -->          
+              
+              <div class="form-group">
+                
+                  <label for="copiaSaberesProyectos">Saberes</label>
+                      <textarea class="form-control" cols="80" rows="6" id="copiaSaberesProyectos" name="copiaSaberesProyectos">
+                  </textarea>
+              </div>
+
+
+            </div>
+            
+          </div>
+          
+          <!--=====================================
+          PIE DEL MODAL
+          ======================================-->
+
+
+
+          <div class="modal-footer">
+
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+           
+
+            <button type="submit" class="btn btn-primary">Copiar Saberes</button>
+
+              <input type="hidden" name="idAlumno" id="idAlumno"> 
+
+          </div>       
+
+         <?php
+
+              $tabla = "quinto";
+              $curso = "5-proyectos-edfisica";
+              $ncurso1 = 11;
+              $ncurso2 = 12;
+              $ncurso3 = 16;
+              $ncurso4 = 17;
+              $periodo = $_SESSION["periodo"];
+
+              $copiaSaberes = new ControladorInformes();
+              $copiaSaberes -> ctrCopiarSaberesProyectos($tabla, $curso, $ncurso1, $ncurso2, $ncurso3, $ncurso4, $periodo);
+
+          ?>
+
+    </form>
+
+          
+    </div>
+
+  </div>
+</div>
+
+
+
